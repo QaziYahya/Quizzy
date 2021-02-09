41e51912bd5f63b1b5670e9a202b29037c1bba65
Qazi Sami ur Rahman <qsami@Qazis-MacBook-Pro.local>
2021-02-09 21:48:20 +0500

    second commit

--- js/app.js
+++ js/app.js
@@ -3,19 +3,20 @@ document.querySelector('.start-btn').addEventListener('click', function () {
   ui.changeState('instructions');
 });
 
-// Event Listener on the quit button
+// Event Listener on the quit button that appears on the instructions state
 document.querySelector('.quit-btn').addEventListener('click', function () {
   ui.changeState('welcome');
 });
 
+// Event Listener on the quit button that appears on the win-lose state
 document.querySelector('.quit-btn-2').addEventListener('click', function () {
   ui.changeState('welcome');
 });
 
-// Event Listener on the start game button
+// Event Listener on the start game button that appears on the instructions state
 document.querySelector('.start-game-btn').addEventListener('click', startGame);
 
-// Event Listener on the start game button
+// Event Listener on the start game button that appears on the win-lose state
 document
   .querySelector('.start-game-btn-2')
   .addEventListener('click', startGame);
@@ -26,69 +27,128 @@ document.querySelector('.next-btn').addEventListener('click', startGame);
 // Event Listener on the Options
 document.querySelector('.answers').addEventListener('click', getAns);
 
+// Used to store the number of the question that we are currently on
 let questionNumber = 0;
 
+/* The data that the api sends is stored in this variable so that we can
+pass it to other functions*/
 let newData = '';
 
+/* We store boolean values in this variable. It is used to stop the timer.
+We want the timer to stop once the user clicks on an option and thats where
+we use this variable. Inside the timer function we hava an if statement
+and it checks the value of stop, if the value of stop = false the timer
+continues running. If at any point the value of stop = true then the timer 
+stops.*/
 let stop;
 
+// Html of the timer. This is used in the timer function.
 let timeLeft = document.querySelector('.time-left');
 
-let i;
+/* This variable is used in the timer function. It used to keep track of the 
+amount of seconds. */
+let i = 20;
 
+/* This variable is also used in the timer function. It is used to clear the 
+interval. Its realtion is with the setInterval function */
 let check;
 
+// Start the game
 function startGame(e) {
+  // Set the value of stop = false so that the timer will run
   stop = false;
+
+  /* Do the following if we are not yet at the 10th question */
   if (questionNumber < 10) {
+    // Get the question from the Api
     http
       .get(
         'https://quizapi.io/api/v1/questions?apiKey=EF97hh7Y1YLCfaOJDTUt7CmzrhXR1fV5XMOQDS2r&category=code&difficulty=Easy&limit=1'
       )
       .then((data) => {
+        /* Remove the borders(if any) of the options and also set the innerHTML
+        = ''*/
         ui.changeToDefault();
+
+        // Change the page state to the game-on state
         ui.changeState('game-on');
+
+        // question data is stored in the newData variable
         newData = data;
-        i = 0;
+
+        // Start the timer from 20 and then go down from there
+        i = 20;
+
+        // Clear any existing interval(if exists)
         clearInterval(check);
+
+        // Run the timer
         timer();
+
+        // Increase the question number
         questionNumber++;
+
+        // Set the question data in the respective fields
         ui.setQuestionData(data, questionNumber);
-        console.log(data);
       });
+
+    /* If we are already at the 10th question then do the following */
   } else {
+    // Change the page state to the win-lose state
     ui.changeState('win-lose');
+
+    // Reset the questionNumber since we reached the end of the game
     questionNumber = 0;
   }
 
   e.preventDefault();
 }
 
+// Timer function
 function timer() {
-  timeLeft.textContent = 0;
+  // Set the value of the timeLeft field to 20 and then go down from there
+  timeLeft.textContent = 20;
+
   check = setInterval(function () {
-    i++;
+    // Decrease the seconds
+    i--;
+    // If stop is false then do the following
     if (!stop) {
-      if (i <= 20) {
+      // If i >= 0 meaning that if we havent reached the end yet then do the following
+      if (i >= 0) {
         timeLeft.textContent = i;
-      } else if (i === 21) {
+
+        // If i<0 meaning that if we reached the end then do the following
+      } else if (i < 0) {
+        // Clear the interval
         clearInterval(check);
+        // highlight the correct answer(because the user ran out of time)
         ui.highlightCorrectAns(newData);
       }
     }
   }, 1000);
 }
 
+/* This function is used to determine which option user clicked on */
 function getAns(e) {
+  /* If stop = true then the timer will be stoped and thats what we want here
+  because the user clicked on an option */
   stop = true;
+
+  // Used to store the textContent of the option user clicked on
   let answerTxt = '';
+
+  // If what the user clicked on is indeed an option then do the following
   if (
     e.target.classList.contains('answer-1') ||
     e.target.classList.contains('answer-2') ||
     e.target.classList.contains('answer-3') ||
     e.target.classList.contains('answer-4')
   ) {
+    // Get the textContent of the clicked option
     answerTxt = e.target.textContent;
+
+    // Check the answer if its corrent or not
     ui.checkAns(newData, answerTxt, e.target);
   }
 }
--- js/ui.js
+++ js/ui.js
@@ -8,6 +8,7 @@ class UI {
 
     // Game On State
     this.gameOnState = document.querySelector('.game-on');
+    // Where we insert the question
     this.question = document.querySelector('.question');
 
     // Win-lose-State
@@ -25,21 +26,17 @@ class UI {
     this.option3 = document.querySelector('.answer-3');
     this.option4 = document.querySelector('.answer-4');
 
-    this.tick1 = document.querySelector('.tick-1');
-    this.tick2 = document.querySelector('.tick-2');
-    this.tick3 = document.querySelector('.tick-3');
-    this.tick4 = document.querySelector('.tick-4');
-
-    this.cross1 = document.querySelector('.cross-1');
-    this.cross2 = document.querySelector('.cross-2');
-    this.cross3 = document.querySelector('.cross-3');
-    this.cross4 = document.querySelector('.cross-4');
-
+    // Where we insert the number of the question
     this.outOf = document.querySelector('.out-of');
 
+    // Used to store the points user scores
     this.points = 0;
   }
 
+  /* When the user clicks on an option then we want the click event to
+  disable meaning that we dont want the user to be able to click any
+  other option and that's what this function does it disables the click
+  event of all the functions */
   disableClickEvent() {
     this.option1.style.pointerEvents = 'none';
     this.option2.style.pointerEvents = 'none';
@@ -47,6 +44,7 @@ class UI {
     this.option4.style.pointerEvents = 'none';
   }
 
+  // This function enables the click event of all the options
   enableClickEvent() {
     this.option1.style.pointerEvents = 'auto';
     this.option2.style.pointerEvents = 'auto';
@@ -54,27 +52,42 @@ class UI {
     this.option4.style.pointerEvents = 'auto';
   }
 
+  /* When the user clicks on an option then that option is given a border based
+  on if the clicked option was the correct option or not and this function is used
+  to remove that border. It removes the border given from the previous question
+  and it also sets the innerHTML of all the options = ''*/
   changeToDefault() {
     this.option1.style.border = 'none';
     this.option1.innerHTML = '';
+
     this.option2.style.border = 'none';
     this.option2.innerHTML = '';
+
     this.option3.style.border = 'none';
     this.option3.innerHTML = '';
+
     this.option4.style.border = 'none';
     this.option4.innerHTML = '';
+
+    // Enable the click event
     this.enableClickEvent();
   }
 
+  // Used to change the state of the page
   changeState(state) {
+    // Do the following for welcome state
     if (state === 'welcome') {
       this.instructionsState.style.display = 'none';
       this.gameOnState.style.display = 'none';
       this.WinOrLose.style.display = 'none';
       this.welcomeState.style.display = 'block';
+
+      // Do the following for Instructions state
     } else if (state === 'instructions') {
       this.welcomeState.style.display = 'none';
       this.instructionsState.style.display = 'block';
+
+      // Do the following for game-on state
     } else if (state === 'game-on') {
       this.instructionsState.style.display = 'none';
       this.WinOrLose.style.display = 'none';
@@ -84,25 +97,54 @@ class UI {
       this.option3.style.display = 'none';
       this.option4.style.display = 'none';
       this.nextBtn.style.display = 'none';
+
+      //Do the following for win-lose state
     } else if ('win-lose') {
       this.gameOnState.style.display = 'none';
+
+      // If the scored 5 or more points then do the following
       if (this.points >= 5) {
+        // Set the textContent of this element to congrats
         this.congratesOrNice.textContent = 'congrats';
+
+        // Set the emoji here
         this.emoji.innerHTML = '<i class="smile-icon far fa-laugh"></i>,';
+
+        // Display the scored points
         this.obtainedPoints.textContent = this.points;
+
+        // If the scored less than 5 then do the following
       } else {
+        // Set the textContent of this element to nice
         this.congratesOrNice.textContent = 'nice';
+
+        // Set the emoji here
         this.emoji.innerHTML = '<i class="smile-icon far fa-laugh-beam"></i>,';
+
+        // Display the scored points
         this.obtainedPoints.textContent = this.points;
       }
+
+      // Display the win-lose container
       this.WinOrLose.style.display = 'block';
+
+      // Reset the points variable since we reached the end
       this.points = 0;
+
+      /* Reset the value of the next button which as changed in the setQuestionData
+      function*/
+      this.nextBtn.value = 'Next';
     }
   }
 
+  /* This function is used to set the question in its respective place and
+  also set all of the options */
   setQuestionData(data, questionNumber) {
+    // Set the question
     this.question.textContent = data[0].question;
 
+    /* If the question only has 2 answers then answer c & answer d will be = to null.
+    So based on that we set the data in only two options */
     if (
       data[0].answers.answer_c === null &&
       data[0].answers.answer_d === null
@@ -111,6 +153,9 @@ class UI {
       this.option1.textContent = data[0].answers.answer_a;
       this.option2.style.display = 'block';
       this.option2.textContent = data[0].answers.answer_b;
+
+      /* If the question only has 3 answers then answer c will != to null but answer d
+      will be = to null. So based on that we set the data in only three options*/
     } else if (
       data[0].answers.answer_c !== null &&
       data[0].answers.answer_d === null
@@ -121,6 +166,9 @@ class UI {
       this.option2.textContent = data[0].answers.answer_b;
       this.option3.style.display = 'block';
       this.option3.textContent = data[0].answers.answer_c;
+
+      /* If the questionhas 4 answers then both anwer c and answer d will not be =
+       to null. So based on that we set the data in all the four options*/
     } else if (
       data[0].answers.answer_c !== null &&
       data[0].answers.answer_d !== null
@@ -135,28 +183,49 @@ class UI {
       this.option4.textContent = data[0].answers.answer_d;
     }
 
+    // Set the number of question in the its respective place
     this.outOf.textContent = questionNumber;
 
+    // If we are at the 10th question then set the value of the next button to 'finish'.
     if (this.outOf.textContent === '10') {
       this.nextBtn.value = 'Finish';
     }
   }
 
+  /* This function is used to highlight the correct answer when the user runs out
+  of time */
   highlightCorrectAns(newData) {
+    /* First disable the click event when the user runs out of time so that the
+    user can't click any of the options */
     this.disableClickEvent();
+
+    /* The next button is hidden by default and we only want it to appear when
+    the user runs out time or clicks an option */
     this.nextBtn.style.display = 'block';
+
+    /* If answer a = true then this means that option a is the correct answer and
+    so we change its border color and also give it that green tick. */
     if (newData[0].correct_answers.answer_a_correct === 'true') {
       this.option1.style.border = '2px solid #00cc00';
       this.option1.innerHTML +=
         '<i class="tick-1 fas fa-check-circle" style="margin-left: 5%"></i>';
+
+      /* If answer b = true then this means that option b is the correct answer and
+      so we change its border color and also give it that green tick. */
     } else if (newData[0].correct_answers.answer_b_correct === 'true') {
       this.option2.style.border = '2px solid #00cc00';
       this.option2.innerHTML +=
         '<i class="tick-1 fas fa-check-circle" style="margin-left: 5%"></i>';
+
+      /* If answer c = true then this means that option c is the correct answer and
+      so we change its border color and also give it that green tick. */
     } else if (newData[0].correct_answers.answer_c_correct === 'true') {
       this.option3.style.border = '2px solid #00cc00';
       this.option3.innerHTML +=
         '<i class="tick-1 fas fa-check-circle" style="margin-left: 5%"></i>';
+
+      /* If answer d = true then this means that option d is the correct answer and
+      so we change its border color and also give it that green tick. */
     } else if (newData[0].correct_answers.answer_d_correct === 'true') {
       this.option4.style.border = '2px solid #00cc00';
       this.option4.innerHTML +=
@@ -164,66 +233,141 @@ class UI {
     }
   }
 
+  /* This function is used to check if the option clicked by the user is correct
+  or not */
   checkAns(newData, answerTxt, selectedAns) {
+    /* First disable the click event so that the user can't click 
+    any other the option */
     this.disableClickEvent();
+
+    /* The next button is hidden by default and we only want it to appear when
+    the user runs out time or clicks an option */
     this.nextBtn.style.display = 'block';
+
+    /* If answer a = true then this means that option a is the correct answer */
     if (newData[0].correct_answers.answer_a_correct === 'true') {
+      /* If answer a is correct then we compare the clicked option to answer a
+      if they match then do the following */
       if (newData[0].answers.answer_a === answerTxt) {
+        /* Give the clicked option a green border */
         selectedAns.style.border = '2px solid #00cc00';
+
+        /* Give the clicked option that green tick */
         selectedAns.innerHTML +=
           '<i class="tick-1 fas fa-check-circle" style="margin-left: 5%"></i>';
+
+        // Increase the points by 1
         this.points++;
+
+        /* If answer a and the clicked option does not match then do the following */
       } else {
+        /* Give the clicked option a red border because its not the correct answer*/
         selectedAns.style.border = '2px solid #ff4c4c';
+
+        /* Give the clicked option that red cross */
         selectedAns.innerHTML +=
           '<i class="cross-3 far fa-times-circle" style="margin-left: 5%"></i>';
 
+        // Give option a a green border because its the correct answer
         this.option1.style.border = '2px solid #00cc00';
+
+        /* Give option a that green tick */
         this.option1.innerHTML +=
           '<i class="tick-1 fas fa-check-circle" style="margin-left: 5%"></i>';
       }
+
+      /* If answer b = true then this means that option b is the correct answer */
     } else if (newData[0].correct_answers.answer_b_correct === 'true') {
+      /* If answer b is correct then we compare the clicked option to answer b
+      if they match then do the following */
       if (newData[0].answers.answer_b === answerTxt) {
+        /* Give the clicked option a green border */
         selectedAns.style.border = '2px solid #00cc00';
+
+        /* Give the clicked option that green tick */
         selectedAns.innerHTML +=
           '<i class="tick-1 fas fa-check-circle" style="margin-left: 5%"></i>';
+
+        // Increase the points by 1
         this.points++;
+
+        /* If answer b and the clicked option does not match then do the following */
       } else {
+        /* Give the clicked option a red border because its not the correct answer*/
         selectedAns.style.border = '2px solid #ff4c4c';
+
+        /* Give the clicked option that red cross */
         selectedAns.innerHTML +=
           '<i class="cross-3 far fa-times-circle" style="margin-left: 5%"></i>';
 
+        // Give option b a green border because its the correct answer
         this.option2.style.border = '2px solid #00cc00';
+
+        /* Give option b that green tick */
         this.option2.innerHTML +=
           '<i class="tick-1 fas fa-check-circle" style="margin-left: 5%"></i>';
       }
+
+      /* If answer c = true then this means that option c is the correct answer */
     } else if (newData[0].correct_answers.answer_c_correct === 'true') {
+      /* If answer c is correct then we compare the clicked option to answer c
+      if they match then do the following */
       if (newData[0].answers.answer_c === answerTxt) {
+        /* Give the clicked option a green border */
         selectedAns.style.border = '2px solid #00cc00';
+
+        /* Give the clicked option that green tick */
         selectedAns.innerHTML +=
           '<i class="tick-1 fas fa-check-circle" style="margin-left: 5%"></i>';
+
+        // Increase the points by 1
         this.points++;
+
+        /* If answer c and the clicked option does not match then do the following */
       } else {
+        /* Give the clicked option a red border because its not the correct answer*/
         selectedAns.style.border = '2px solid #ff4c4c';
+
+        /* Give the clicked option that red cross */
         selectedAns.innerHTML +=
           '<i class="cross-3 far fa-times-circle" style="margin-left: 5%"></i>';
 
+        // Give option c a green border because its the correct answer
         this.option3.style.border = '2px solid #00cc00';
+
+        /* Give option c that green tick */
         this.option3.innerHTML +=
           '<i class="tick-1 fas fa-check-circle" style="margin-left: 5%"></i>';
       }
+
+      /* If answer d = true then this means that option d is the correct answer */
     } else if (newData[0].correct_answers.answer_d_correct === 'true') {
+      /* If answer d is correct then we compare the clicked option to answer d
+      if they match then do the following */
       if (newData[0].answers.answer_d === answerTxt) {
+        /* Give the clicked option a green border */
         selectedAns.style.border = '2px solid #00cc00';
+
+        /* Give the clicked option that green tick */
         selectedAns.innerHTML +=
           '<i class="tick-1 fas fa-check-circle" style="margin-left: 5%"></i>';
+
+        // Increase the points by 1
         this.points++;
+
+        /* If answer d and the clicked option does not match then do the following */
       } else {
+        /* Give the clicked option a red border because its not the correct answer*/
         selectedAns.style.border = '2px solid #ff4c4c';
+
+        /* Give the clicked option that red cross */
         selectedAns.innerHTML +=
           '<i class="cross-3 far fa-times-circle" style="margin-left: 5%"></i>';
 
+        // Give option d a green border because its the correct answer
         this.option4.style.border = '2px solid #00cc00';
+
+        /* Give option d that green tick */
         this.option4.innerHTML +=
           '<i class="tick-1 fas fa-check-circle" style="margin-left: 5%"></i>';
       }
@@ -231,4 +375,5 @@ class UI {
   }
 }
 
+// New ui Object
 const ui = new UI();
